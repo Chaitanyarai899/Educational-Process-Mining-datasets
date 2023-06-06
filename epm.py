91ff3d972b32f8f83bc7207ac295c5a84d56b035
Ajay <112794893+Ajay-308@users.noreply.github.com>
2023-06-06 14:08:58 +0530

    Update epm.py

--- epm.py
+++ epm.py
@@ -1,30 +1,35 @@
 import streamlit as st
 import pickle
-import pandas as pd
 import numpy as np
 
-model = pickle.load(open(r"C:\Users\mraja\Downloads\ml project\educte.pkl", 'rb'))
+model = pickle.load(open(r"C:\Users\mraja\Downloads\ml project\edupm.pkl", 'rb'))
 
 st.title('Epm pass-fail predictor')
 
 cols = []
-marks = []
 
-features_to_drop = ['student id', 'total point', 'es3.1']
+features_to_drop = ['student id', 'total marks']
 
-for i in range(17):
+for i in range(16):
     if i+1 in features_to_drop:
         continue
-    col = st.number_input(f"Enter lab marks {i+1}")
+    col = st.number_input(f"Enter lab marks {i+1}", value=0.0, step=0.1)
     cols.append(col)
+
+total_marks = st.number_input("Enter total marks", value=0.0, step=0.1)
+
+cols.append(total_marks)
 
 if st.button('Predict pass/fail'):
     marks = np.array(cols).reshape(1, -1)
-    prediction = model.predict(marks)
 
-    if prediction == 0:
-        st.write('Result: Good & pass')
-    elif prediction == 1:
-        st.write('Result: fair & pass')
+    if all(cols):
+        prediction = model.predict(marks)
+        if prediction == 0:
+            st.write('Result: Good & pass')
+        elif prediction == 1:
+            st.write('Result: fair & pass')
+        else:
+            st.write('Result: weak & fail')
     else:
-        st.write('Result: weak & fail')
+        st.error("Please fill in all the required fields.")
